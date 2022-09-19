6e6d21a19af2fcf1066748023e6cf403b0309703
Pratik <chougulepratik07@gmail.com>
2022-09-19 02:23:43 -0700

    LoginViewController File added

---
+++ GitHubDemo/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  GitHubDemo
+//
+//  Created by Capgemini-DA161 on 9/19/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Log From Login View Controller")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
