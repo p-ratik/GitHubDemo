4cda126050076ffc20a90adb320d7aa3f324590d
Pratik <chougulepratik07@gmail.com>
2022-09-19 02:12:47 -0700

    Log Added

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("GitHub Demo")
+        print("Log Added By Pratik")
         
     }
 
